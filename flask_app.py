acf3ed8a0bbe62a76627b353236c7db3c62b5696
Kev <93ly.kevin@gmail.com>
2019-09-13 22:04:38 +0000

    MADE WEBSITE RECEIVE DYNAMIC SEARCH

--- flask_app.py
+++ flask_app.py
@@ -15,45 +15,40 @@ app = Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
 
-SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
-    username="KevTLy",
-    password="fluffykins123",
-    hostname="KevTLy.mysql.pythonanywhere-services.com",
-    databasename="KevTLy$comments",
-)
-app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
-app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+# SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+#     username="KevTLy",
+#     password="fluffykins123",
+#     hostname="KevTLy.mysql.pythonanywhere-services.com",
+#     databasename="KevTLy$comments",
+# )
+# app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+# app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+# app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 
-class Comment(db.Model):
+# class Comment(db.Model):
 
-    __tablename__ = "comments"
+#     __tablename__ = "comments"
 
-    id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(4096))
 
 # comments = []
 
 # @app.route("/")
 @app.route("/", methods=["GET", "POST"])
 @cross_origin()
-def stockx_search():
-    supreme = jsonify(stockx.search('Supreme'))
+def searchStockx():
+    if request.method == 'POST':
+        searchText = request.data
+        print(searchText)
+        searchRes = jsonify(stockx.search(searchText))
+        print(searchRes)
+        return searchRes
+    return jsonify('GET instead')
 
-    # supreme.headers.add('Access-Control-Allow-Origin', '*')
-    return supreme
-
-    # return request.post('
-
-    # supreme = pd.DataFrame(supreme)
-    # return jsonify(supreme.head())
-# def index():
-#     if request.method == "GET":
-#         # return render_template("main_page.html", comments=Comment.query.all())
-
-# comment = Comment(content=request.form["contents"])
-#     db.session.add(comment)
-#     db.session.commit()
-#     return redirect(url_for('index'))
+# @app.route("/", methods=["GET"])
+# @cross_origin()
+# def stockx_get():
+#     return jsonify('in GET request')
